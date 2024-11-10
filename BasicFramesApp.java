e833f853bdf0208964d584d6746b960fd2f7623d
CrackedPhos <mpramarumo@icloud.com>
2024-11-10 11:13:13 +0200

    Add files via upload

---
+++ BasicFramesApp.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package basicframesapp;
+
+import za.ac.tut.MyFirstFrame;
+
+/**
+ *
+ * @author 27764
+ */
+public class BasicFramesApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MyFirstFrame myFrame = new MyFirstFrame();
+        
+        
+    }
+    
+}
